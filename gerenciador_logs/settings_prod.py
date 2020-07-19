63d3bc7082005a80cf78d62df69da6daf943eb2a
Amanda Gundes de Almeida <amanda.almeida@softplan.com.br>
2020-07-19 20:49:59 -0300

    Arrumando arquivo de configuração

--- gerenciador_logs/settings_prod.py
+++ gerenciador_logs/settings_prod.py
@@ -7,8 +7,12 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
+
+# Heroku Statics Hack (não é muito recomendado para produção!!)
+# https://devcenter.heroku.com/articles/django-assets
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MIDDLEWARE.append('whitenoise.middleware.WhiteMoiseMiddleWare')
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
